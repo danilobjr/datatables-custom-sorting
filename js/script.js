c9bd54a917096d29858d4eb666f65101a35ece40
Danilo Jr <danilobjr@gmail.com>
2014-12-21 21:36:29 -0300

    added demo page

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(function () {
+    'use strict';
+
+    $('[type=checkbox], [type=radio]').iCheck({
+        checkboxClass: 'icheckbox_flat-blue',
+        radioClass: 'iradio_flat-blue'
+    });
+
+    dataTablesFactory
+        // .selector('#special')
+        // .setup({
+        //     "sPaginationType": "full_numbers"
+        // })
+        .instantiate();
+});
